682595554a779c1d9d61632de8c21205ef91f0f6
Mathew Fedrick <105207473+Mathew1327@users.noreply.github.com>
2025-08-04 12:18:17 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
+// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // ✅ Add this
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter> {/* ✅ Wrap your app */}
+    <BrowserRouter> {/* ✅ This is the only place BrowserRouter should be */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
